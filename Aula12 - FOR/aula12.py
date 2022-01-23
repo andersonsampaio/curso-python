d3b734606c69fcb6c63c97c7bd38d0662193d556
andersonsampaio <96148499+andersonsampaio@users.noreply.github.com>
2022-01-23 13:40:19 -0300

    New

--- Aula12 - FOR/aula12.py
+++ Aula12 - FOR/aula12.py
@@ -1,5 +1,7 @@
 #======================
 #Parte 1
+print("==================")
+print("Inicio parte 01")
 carros=["HRV","Golf","Argo","Focus"]
 
 for x in carros: # cria um elemento 'x' para receber o valor de uma lista 'carros no ex'
@@ -9,16 +11,18 @@ for x in carros: # cria um elemento 'x' para receber o valor de uma lista 'carro
 
 #======================
 #Parte 2
+print("\n==================")
+print("Inicio parte 02")
 carros=["HRV","Golf","Argo","Focus"]
 
 for x in carros: # cria um elemento 'x' para receber o valor de uma lista 'carros no ex'
-    print(x)
     if(x=="Argo"):
         break
-
+    print(x)
 
 #======================
 #Parte 3
-
+print("\n==================")
+print("Inicio parte 03")
 for x in "CBF cursos": # cria um elemento 'x' para receber o valor de cada posição da string
     print(x)
