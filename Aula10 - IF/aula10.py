e1558c69790b23eb3b251fa4b4c5c8bec2e7919a
andersonsampaio <96148499+andersonsampaio@users.noreply.github.com>
2022-01-23 12:13:55 -0300

    NEW

---
+++ Aula10 - IF/aula10.py
@@ -0,0 +1,21 @@
+from pickle import TRUE
+
+
+a=10
+b=5
+res=0
+op="/"
+
+if op=="+":
+    res=a+b
+
+if op=="-":
+    res=a-b
+
+if op=="*":
+    res=a*b
+
+if op=="/":
+    res=a/b
+
+print(str(a) + op + str(b) + " = " + str(res)) 
---
+++ Aula11 - IF Else/aula11.py
@@ -0,0 +1,33 @@
+#==============
+# #Parte 01
+a=10
+b=5
+res=0
+op="+"
+
+if op=="+": # 'ELIF'(ELSE + IF) só verifica se o IF ou ELIF anterior ñ for verdadeiro
+    res=a+b
+elif op=="-":
+    res=a-b
+elif op=="*":
+    res=a*b
+elif op=="/":
+    res=a/b
+else: # 'else' se nenhuma condição anterior satisfazer já entra nele
+    print("Operador invalido")
+
+print(str(a) + op + str(b) + " = " + str(res)) 
+
+#==============
+# #Parte 02
+clima="sol"
+dinheiro=301
+lugar=""
+
+if clima=="sol" and (dinheiro>300 and dinheiro<=500):
+    lugar="clube"
+else:
+    lugar="cinema"
+
+print("\nVou ao "+lugar)
+
