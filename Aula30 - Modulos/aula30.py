71fcbed3e4bc0a6734279100c613be39983a57a9
andersonsampaio <96148499+andersonsampaio@users.noreply.github.com>
2022-02-05 13:46:01 -0300

    New

---
+++ Aula30 - Modulos/aula30.py
@@ -0,0 +1,4 @@
+import canal as cn
+
+cn.canal_nome()
+print(cn.jogador["nome"])
---
+++ Aula30 - Modulos/canal.py
@@ -0,0 +1,9 @@
+def canal_nome():
+    print("CBF Cursos")
+
+jogador={
+    "nome":"Bruce",
+    "pontos":100,
+    "energia":75,
+    "time":1
+}
