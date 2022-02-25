b9cc42867e26c368d3a94714c56a7477d8d819c8
andersonsampaio <96148499+andersonsampaio@users.noreply.github.com>
2022-02-25 11:55:31 -0300

    New

--- Aula03 - var global/aula03.py
+++ Aula03 - var global/aula03.py
@@ -2,11 +2,13 @@ from xml.etree.ElementTree import canonicalize
 
 
 num1=num2=res=0
-
+B =3
 def cn():
-    global canal 
+    global canal,c 
     canal="CBF Cursos"
+    c = B + 3
 
 cn()
 
 print(canal)
+print(c)
